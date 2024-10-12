28b5a1fc2a04f29da6b7dced6f2295ab36dea80c
finsterdexter <finster@finster.xyz>
2024-10-12 08:15:39 -0500

    Rename parse_data to export_json

--- export_json.py
+++ export_json.py
@@ -46,39 +46,6 @@ with open("output/skills.json", "w", encoding="utf-8") as output:
 	# print(junk)
 
 
-# all_adept_powers = []
-# with open("data/adept.dat") as adept_file:
-# 	types = dict()
-# 	category_tree = list()
-# 	current_category_level = 0
-	
-# 	for line in adept_file:
-# 		line = line.rstrip()
-# 		if (line.startswith("!")): # comment
-# 			continue
-		
-# 		# this is a type definition
-# 		if (line.startswith("0-")):
-# 			this_type = parse_functions.type_dict(line)
-# 			types[this_type["type_id"]] = this_type
-# 			continue
-
-# 		try:
-# 			line.index("|")
-# 		except ValueError:
-# 			# this is a category tree branch
-# 			(category_tree, current_category_level) = parse_functions.parse_category_tree_branch(line, category_tree, current_category_level)
-# 			continue
-
-# 		# process an actual object
-# 		splits = re.split(r"\|", line)
-# 		if (splits[0].startswith("2-* ") == False):
-# 			raise Exception("Unexpected line: " + line)
-# 		name_type = splits[0].lstrip("2-* ")
-# 		name_type_split = name_type.rsplit(None, 1)
-# 		name = name_type_split[0]
-# 		type_id = name_type_split[1]
-# 		all_adept_powers.append(AdeptPower(name, splits[1], splits[2], splits[3], splits[4].strip(), category_tree))
 all_adept_powers = parse_functions.parse_file("data/adept.dat")
 with open("output/adept_powers.json", "w", encoding="utf-8") as output:
 	json.dump(all_adept_powers, output, indent=2, default=lambda o: o.__dict__)
